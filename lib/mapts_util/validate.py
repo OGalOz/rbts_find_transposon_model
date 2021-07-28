2176bc66916da6d21a4641386f163bfa12c7da43
Omree Gal-Oz <ogaloz@lbl.gov>
2021-07-28 15:27:50 -0700

    Transposon Model -> TnSeq Model

--- lib/mapts_util/validate.py
+++ lib/mapts_util/validate.py
@@ -13,8 +13,16 @@ def validate_params(params):
             raise Exception(f" {x} must be part of params."
                             " Params: " + ", ".join(params.keys()))
 
-    if params["maxReads"] is None:
+    if params["maxReads"] is None or params["maxReads"] == "":
         params["maxReads"] = 600000
+    if params["minQuality"] is None or params["minQuality"] == "":
+        params["minQuality"] = 10
+
+    for x in ["maxReads", "minQuality"]:
+        if isinstance(params[x],str):
+            if "," in params[x]:
+                params[x] = params[x].replace(",","")
+            params[x] = int(params[x])
 
     if not isinstance(params["minQuality"], int):
             raise Exception(f"Param minQuality must be passed as an int."
